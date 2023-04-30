276a86075ea887ab7d65ea2f780e03697efda913
RAFIQ-CLOUD <124174722+RAFIQ-CLOUD@users.noreply.github.com>
2023-05-01 02:59:07 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,12 +1,12 @@
 if [ -z $UPSTREAM_REPO ]
 then
   echo "Cloning main Repository"
-  git clone https://github.com/MS-BOTZZZ/Vip2.git /Vip2
+  git clone https://github.com/RAFIQ-CLOUD/MR-X-SHEIK-FILTER-BOT-PROX.git /MR-X-SHEIK-FILTER-BOT-PROX
 else
   echo "Cloning Custom Repo from $UPSTREAM_REPO "
-  git clone $UPSTREAM_REPO /Vip2
+  git clone $UPSTREAM_REPO /MR-X-SHEIK-FILTER-BOT-PROX
 fi
-cd /Vip2
+cd /MR-X-SHEIK-FILTER-BOT-PROX
 pip3 install -U -r requirements.txt
 echo "🍃Starting Bot🍃...."
 python3 bot.py
